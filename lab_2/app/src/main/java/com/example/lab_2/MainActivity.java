dd4b2a76905170c0e6bb68506ec4e929da8bb032
KotSlava <kot.slava.mih@gmail.com>
2021-10-23 14:03:40 +0300

    commit

---
+++ lab_2/app/src/main/java/com/example/lab_2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import android.content.res.Configuration;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
